14aaf2f5403c35d152722689038114ee11247b27
geoffrey <geoffrey.maillot@gmail.com>
2022-03-05 14:30:11 +0100

    Add Headers Access Control

--- src/api/index.js
+++ src/api/index.js
@@ -3,4 +3,7 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://kanbanlike.herokuapp.com/api',
   timeout: 3000,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
 });
